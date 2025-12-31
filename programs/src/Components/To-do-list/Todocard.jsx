a6e9e718c623c1e006056fbfd28aaa0ba328bd24
sachin-10-alt <sv3629285@gmail.com>
2025-12-31 12:30:02 +0530

    madechanges

---
+++ programs/src/Components/To-do-list/Todocard.jsx
@@ -0,0 +1,10 @@
+export default function Todocard() {
+  return (
+  <div>
+    <h2>TO-DO-LIST</h2>
+    <input type="text" placeholder="Add your new task" />
+    <button>Add Task</button>
+    <div>no task yet</div>
+    </div>
+    );
+}
